9a4435db3dc609b6f0e594b2c43441739616beb2
Tom Booth <tombooth@gmail.com>
2013-10-03 14:33:53 +0000

    Add a plugin for storing message history

---
+++ plugins/history.js
@@ -0,0 +1,28 @@
+
+var fs = require('fs');
+
+module.exports = function(config, irc, www) {
+
+  if (!config.path) {
+    console.error('You need to provide out_path on the config for this plugin, relative to the CWD.');
+    return;
+  }
+
+  var outStream = fs.createWriteStream(process.cwd() + '/' + config.path, { encoding: 'utf8' });
+
+  irc.addListener('message', function(nick, to, text, message) {
+    
+    var log = JSON.stringify({
+      nick: nick,
+      to: to,
+      text: text,
+      message: message
+    });
+
+    outStream.write(log, 'utf8');
+
+  });
+
+};
+
+
