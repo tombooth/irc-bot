f07b374de3edd54b42d96eb62c811d07ae45be90
Tom Booth <tombooth@gmail.com>
2013-10-03 15:11:44 +0000

    Now there is bodyParser() use that

--- plugins/github.js
+++ plugins/github.js
@@ -5,12 +5,7 @@ module.exports = function(config, irc, www) {
   www.post('/github', function(request, response) {
     irc.say('Github POSTd');
 
-    request.on('data', function(d) {
-      if (d) { console.log(d.toString('utf8')); }
-    });
-    request.on('close', function() {
-      response.end();
-    });
+    console.log(request.body);
   });
 
 };
