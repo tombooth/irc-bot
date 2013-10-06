895ad7f412118ec8f6dbb4233e2d45ebdaeb6590
Tom Booth <tombooth@gmail.com>
2013-10-06 22:20:11 +0100

    Add allowed change test for pivotal

--- tests/plugins/pivotal-test.js
+++ tests/plugins/pivotal-test.js
@@ -5,7 +5,8 @@ var sinon = require('sinon'),
     pivotalPlugin = require('../../src/plugins/pivotal.js');
 
 
-var changeJSON = JSON.parse(fs.readFileSync(__dirname + '/fixtures/pivotal-change.json')),
+var finishedJSON = JSON.parse(fs.readFileSync(__dirname + '/fixtures/pivotal-finished.json')),
+    startedJSON = JSON.parse(fs.readFileSync(__dirname + '/fixtures/pivotal-started.json')),
     bot;
 
 
@@ -32,11 +33,26 @@ exports.testChange = function(test) {
 
   pivotalPlugin({ }, bot, function() { });
 
-  testUtil.webHookRequest(bot, { body: changeJSON });
+  testUtil.webHookRequest(bot, { body: finishedJSON });
 
   test.ok( bot.say.calledOnce );
   test.equal( bot.say.getCall(0).args[0],
       'Data In: Notify on 5xx errors (Tom Booth finished). http://www.pivotaltracker.com/story/show/55989114');
+
+  test.done();
+
+};
+
+exports.testChangeAllowed = function(test) {
+
+  pivotalPlugin({ allow: ['started'] }, bot, function() { });
+
+  testUtil.webHookRequest(bot, { body: finishedJSON });
+  testUtil.webHookRequest(bot, { body: startedJSON });
+
+  test.ok( bot.say.calledOnce );
+  test.equal( bot.say.getCall(0).args[0],
+      'Data In: Notify on 5xx errors (Tom Booth started). http://www.pivotaltracker.com/story/show/55989114');
 
   test.done();
 
