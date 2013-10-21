7851afd614e5a61ee6774e3afdc2d0e5e7c1f189
Tom Booth <tombooth@gmail.com>
2013-10-22 00:27:36 +0100

    Add note about word response plugin

--- src/plugins/wordResponse.js
+++ src/plugins/wordResponse.js
@@ -2,6 +2,7 @@
 
 module.exports = function(config, bot, done) {
 
+  // currently only sends a response if addressed to bot
   Object.keys(config).forEach(function(word) {
 
     var response = config[word];
